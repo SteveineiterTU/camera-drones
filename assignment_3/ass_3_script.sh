4c4a1b400a7307228063d2eca804e6ef86c4c6cd
Stefan Schoerkmeier <s.schoerkmeier@gmx.at>
2024-01-22 15:33:45 +0100

    small fixes, should be possible to run now

--- assignment_3/ass_3_script.sh
+++ assignment_3/ass_3_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # PARAMETERS
-SESSION_NAME="ass_2"
+SESSION_NAME="ass_3"
 TMUX='byobu' #Options: tmux | byobu
 SOURCE='/home/student/.bashrc'
 
@@ -33,10 +33,10 @@ then
 
   #    Path planner ros node
   ${TMUX} send-keys -t ${SESSION_NAME}:1.0 'source $HOME/.bashrc' C-m
-  ${TMUX} send-keys -t ${SESSION_NAME}:1.0 'rosrun octomap_path_planner octomap_path_planner_ros_node trajectory_visualization --runtime 15 --planner RRTStar -o WeightedLengthAndClearanceCombo -f planner_trajectory.txt --info 2' C-m
+  ${TMUX} send-keys -t ${SESSION_NAME}:1.0 'rosrun octomap_path_planner octomap_path_planner_ros_node trajectory_visualization --runtime 15 --planner RRTStar -o WeightedLengthAndClearanceCombo -f planner_trajectory.txt --info 2 --octomap /home/student/camera-drones/catkin_ws/src/octomap_path_planner/maps/power_plant.bt' C-m
   #    Octomap server 
   ${TMUX} send-keys -t ${SESSION_NAME}:1.2 'source $HOME/.bashrc' C-m
-  ${TMUX} send-keys -t ${SESSION_NAME}:1.2 'roslaunch octomap_path_planner octomap_mapping_a2.launch --octomap:=$HOME/camera-drones/catkin_ws/src/octomap_path_planner/maps/power_plant.bt --wait' C-m
+  ${TMUX} send-keys -t ${SESSION_NAME}:1.2 'roslaunch octomap_path_planner octomap_mapping_a2.launch octomap:=$HOME/camera-drones/catkin_ws/src/octomap_path_planner/maps/power_plant.bt --wait' C-m
   #    Set a local transform so we can use world.
   ${TMUX} send-keys -t ${SESSION_NAME}:1.3 'source $HOME/.bashrc' C-m
   ${TMUX} send-keys -t ${SESSION_NAME}:1.3 'rosrun tf static_transform_publisher 0 0 0 0 0 0 1 world my_frame 10' C-m
@@ -48,18 +48,19 @@ then
   ${TMUX} send-keys -t ${SESSION_NAME}:1.5 'rosrun rviz rviz' C-m
 
   # Send keys (see README in octomap path planner package for nice points)
-  ${TMUX} send-keys -t ${SESSION_NAME}:1.1 'source $HOME/.bashrc' C-m
-  ${TMUX} send-keys -t ${SESSION_NAME}:1.1 'rostopic pub /octomap_path_planner/current_position geometry_msgs/Point "x: -14	
-y: -8 
+  ${TMUX} send-keys -t ${SESSION_NAME}:1.6 'source $HOME/.bashrc' C-m
+  ${TMUX} send-keys -t ${SESSION_NAME}:1.6 'rostopic pub /octomap_path_planner/current_position geometry_msgs/Point "x: 14	
+y: 8 
 z: 5" --once' C-m
   sleep 4s
-  ${TMUX} send-keys -t ${SESSION_NAME}:1.1 'source $HOME/.bashrc' C-m
-  ${TMUX} send-keys -t ${SESSION_NAME}:1.1 'rostopic pub /octomap_path_planner/goal_position geometry_msgs/Point "x: 28
+  # ${TMUX} send-keys -t ${SESSION_NAME}:1.6 'source $HOME/.bashrc' C-m
+  # ${TMUX} send-keys -t ${SESSION_NAME}:1.6 'rostopic pub /octomap_path_planner/goal_position geometry_msgs/Point "x: 28
 y: 8
 z: 1" --once' C-m
+  sleep 4s
   # Trajectory Sampler
-  # ${TMUX} send-keys -t ${SESSION_NAME}:1.4 'source $HOME/.bashrc' C-m
-  #${TMUX} send-keys -t ${SESSION_NAME}:1.4 'roslaunch trajectory_planner trajectory_planner.launch --dt:=0.01' C-m  
+  ${TMUX} send-keys -t ${SESSION_NAME}:1.1 'source $HOME/.bashrc' C-m
+  ${TMUX} send-keys -t ${SESSION_NAME}:1.1 'roslaunch trajectory_planner trajectory_planner.launch dt:=0.01' C-m  
 
 
   # Select unused window so we can debug (eg rostopic, rosnode, ..)
